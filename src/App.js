d500f3821b74d7e80f2eee259a05df17d26b8339
Travis Leonard <travis.leonard.tl@gmail.com>
2023-09-27 21:25:13 -0600

    added new App.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,58 +1,147 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+'use client';
+import { useEffect, useState } from 'react';
 
-function App() {
+export default function Home() {
+  let totalGridSize = 20;
+
+  let initialSnakePosition = [
+    { x: totalGridSize / 2, y: totalGridSize / 2 },
+    { x: totalGridSize / 2 + 1, y: totalGridSize / 2 }
+  ];
+
+  const [food, setFood] = useState({ x: 5, y: 5 })
+  const [snake, setSnake] = useState(initialSnakePosition);
+  const [direction, setDirection] = useState('LEFT');
+  const [score, setScore] = useState(0);
+
+  function renderBoard() {
+    let cellArray = [];
+    for (let row = 0; row < totalGridSize; row++) {
+      for (let col = 0; col < totalGridSize; col++) {
+        let className = 'cell';
+
+        let isFood = food.x === row && food.y === col;
+
+        let isSnake = snake.some(ele => ele.x === row && ele.y === col);
+
+        let isSnakeHead = snake[0].x === row && snake[0].y === col;
+
+        if (isFood) {
+          className = className + ' food';
+        }
+
+        if (isSnake) {
+          className = className + ' snake';
+        }
+
+        if (isSnakeHead) {
+          className = className + ' snakeHead'
+        }
+
+        let cell = <div className={className} key={`${row}-${col}`}></div>;
+        cellArray.push(cell);
+      }
+    }
+
+    return cellArray;
+  }
+
+  function gameOver() {
+    setSnake(initialSnakePosition)
+    setScore(0)
+  }
+
+  function updateGame() {
+
+    if ( snake[0].x < 0 ||
+       snake[0].x >20 || 
+       snake[0].y < 0 || 
+       snake[0].y >20 
+       ) { 
+        gameOver();
+        return;
+      }
+
+    let isBit = snake.slice(1).some (ele => ele.x === snake[0].x && ele.y === snake[0].y);
+
+    if (isBit) {
+      gameOver();
+      return
+    }
+
+    let newSnake = [...snake];
+
+    switch (direction) {
+      case 'LEFT':
+        newSnake.unshift({ x: newSnake[0].x, y: newSnake[0].y - 1 });
+        break;
+      case 'RIGHT':
+        newSnake.unshift({ x: newSnake[0].x, y: newSnake[0].y + 1 });
+        break;
+      case 'UP':
+        newSnake.unshift({ x: newSnake[0].x - 1, y: newSnake[0].y });
+        break;
+      case 'DOWN':
+        newSnake.unshift({ x: newSnake[0].x + 1, y: newSnake[0].y });
+        break;
+    }
+
+    let isAteFood = newSnake[0].x === food.x && newSnake[0].y === food.y;
+
+    if (isAteFood) {
+      setScore((prev) => prev + 1);
+      renderFood();
+    } else {
+      newSnake.pop();
+    }
+
+    
+    setSnake(newSnake);
+  }
+
+  function updateDirection(e) {
+    let code = e.code;
+
+    switch (code) {
+      case 'ArrowUp':
+        if (direction !== 'DOWN') setDirection('UP');
+        break;
+      case 'ArrowDown':
+        if (direction !== 'UP') setDirection('DOWN');
+        break;
+      case 'ArrowLeft':
+        if (direction !== 'RIGHT') setDirection('LEFT');
+        break;
+      case 'ArrowRight':
+        if (direction !== 'LEFT') setDirection('RIGHT');
+        break;
+    }
+  }
+
+  function renderFood() {
+    let xPosition = Math.floor(Math.random() * totalGridSize);
+    let yPosition = Math.floor(Math.random() * totalGridSize);
+
+    setFood({x: xPosition, y: yPosition})
+  }
+
+  useEffect(() => {
+    let interval = setInterval(updateGame, 250);
+    return () => clearInterval(interval, updateGame);
+  });
+
+  useEffect(() => {
+    document.addEventListener('keydown', updateDirection);
+    return () => clearInterval('keydown', updateDirection);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className='container'>
+      <div className='score'>
+        Score : <span>{score}</span>
+      </div>
+      <div className='board'>{renderBoard()}</div>
     </div>
   );
 }
 
-export default App;
